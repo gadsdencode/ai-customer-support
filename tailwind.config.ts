5741a4c193264c6ccd2b82757416b599b035d580
gadsdencode <jordan.martens@osscontact.com>
2024-12-08 07:53:24 -0500

    added sidebar and info modal info modal trigger in sidebar info modal gives quick start guide to using the AI Assistant

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,31 +68,39 @@ export default {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		keyframes: {
-			"accordion-down": {
-			  from: { height: "0" },
-			  to: { height: "var(--radix-accordion-content-height)" },
-			},
-			"accordion-up": {
-			  from: { height: "var(--radix-accordion-content-height)" },
-			  to: { height: "0" },
-			},
-			shimmer: {
-			  "0%": {
-				backgroundPosition: "200% 0",
-				opacity: "1",
-			  },
-			  "100%": {
-				backgroundPosition: "-200% 0",
-				opacity: "0",
-			  },
-			},
-		  },
-		  animation: {
-			"accordion-down": "accordion-down 0.2s ease-out",
-			"accordion-up": "accordion-up 0.2s ease-out",
-			shimmer: "shimmer 2s ease-in-out forwards",
-		  },
-		},
-	  },
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			},
+  			shimmer: {
+  				'0%': {
+  					backgroundPosition: '200% 0',
+  					opacity: '1'
+  				},
+  				'100%': {
+  					backgroundPosition: '-200% 0',
+  					opacity: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+  			shimmer: 'shimmer 2s ease-in-out forwards'
+  		}
+  	}
+  },
 	  plugins: [animate],
 	} satisfies Config;
