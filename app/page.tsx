16109d1c2b542bf7f599a8c1ea36a77b3ce011f2
Jordan <113121510+gadsdencode@users.noreply.github.com>
2024-12-10 12:27:51 -0500

    Update page.tsx
    removed footer links

--- app/page.tsx
+++ app/page.tsx
@@ -13,36 +13,6 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://overture-systems.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Overture Systems Solutions
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://kainbridge.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Kainbridge
-        </a>
       </footer>
     </div>
   );
